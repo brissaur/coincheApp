e417c573e1b972dc4da2e6d13832cdea85705920
BRISSAUD <robin.brissaud@capgemini.com>
2016-01-20 10:25:58 +0100

    Clean initial code

--- js/chat.js
+++ js/chat.js
@@ -1,39 +1,47 @@
+// ==============================================================
+// ================== GLOBAL VARS ===============================
+// ==============================================================
 var socket = io();
 var dealer;
 // var players = [];
 var places = {};
 var pseudo = '';
+
+// <<<<<<<<<<<< Send chat message >>>>>>>>>>>>>>
 $('form').submit(function(){
   socket.emit('chat_message', {message: $('#m').val()});
   $('#m').val('');
   return false;
 });
 
+// <<<<<<<<<<<< Receive chat message >>>>>>>>>>>>>>
 socket.on('chat_message', function(msg){
   $('#messages').append($('<li>').text(msg.name + ': ' +msg.message));//TODO EVOL scroll down auto
 });
+// <<<<<<<<<<<< Manage authentification >>>>>>>>>>>>>>
 socket.on('identification_required', function(msg){
   console.log(pseudo);
   while(pseudo=='' ||pseudo==null){pseudo = prompt('What is your pseudo?');}//KO
   socket.emit('connection', {name: pseudo}); 
 });
+// <<<<<<<<<<<< Manage new connection >>>>>>>>>>>>>>
 socket.on('connection', function(msg){
   $('#messages').append($('<li>').text(msg.name + ' is connected.'));
 });
+// <<<<<<<<<<<< Manage new disconnection >>>>>>>>>>>>>>
 socket.on('disconnection', function(msg){
   $('#messages').append($('<li>').text(msg.name + ' disconnected.'));
 });
 
+// <<<<<<<<<<<< Manage my turn to play >>>>>>>>>>>>>>
 socket.on('play', function(msg){
-  // $('#messages').append($('<li>').text('game start with player ' + msg.name));
-    // console.log('My turn to play...');
     var card = prompt('What do you want to play?');
     socket.emit('play', {card: card});
 });
+// <<<<<<<<<<<< Manage game initialization >>>>>>>>>>>>>>
 socket.on('initialize_game', function(msg){
   $('#messages').append($('<li>').text('Starting Game...'));
   // var dealer = msg.dealer;//TODO button
-  // // var players = msg.players;
   var myIndex = msg.players.indexOf(pseudo);
   var positions = ['bottomPlayer', 'leftPlayer', 'topPlayer', 'rightPlayer'];
   for (var i = 0; i <4; i++) {
@@ -43,12 +51,11 @@ socket.on('initialize_game', function(msg){
     document.getElementById(positions[i]).childNodes[1].innerHTML=playername;
   };
 });
+// <<<<<<<<<<<< Manage a player played >>>>>>>>>>>>>>
 socket.on('played', function(msg){
-  // $('#messages').append($('<li>').text(msg.name + ' played ' + msg.card));
-  // debugger;
   document.getElementById(places[msg.name]).childNodes[0].src='/images/cards/'+msg.card+'.png';
-  // $(places[name]+' img').src='/images/cards/'+ msg.card +'.png';
 });
+// <<<<<<<<<<<< Manage end of a trick >>>>>>>>>>>>>>
 socket.on('end_trick', function(msg){
   $('#messages').append($('<li>').text(msg.message));
   for(divs in places){
